371a004db1328cc515b445e7dee9ad88152723ad
Pedro <pedro_hga@proton.me>
2024-06-10 22:23:06 -0300

    changed wezterm to solarized

--- .wezterm.lua
+++ .wezterm.lua
@@ -6,12 +6,13 @@ local config = wezterm.config_builder()
 
 -- config
 
-config.color_scheme = "terafox"
+config.color_scheme = "Solarized Dark (Gogh)"
+-- config.color_scheme = "terafox"
 
 config.font = wezterm.font("Iosevka Nerd Font", { weight = "Medium" })
 -- config.font = wezterm.font("Martian Mono Condensed")
-config.font_size = 12
-config.line_height = 1.125
+config.font_size = 12.5
+config.line_height = 1.25
 
 config.hide_tab_bar_if_only_one_tab = true
 config.window_decorations = "NONE"
