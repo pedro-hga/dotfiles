404b7a558900e74cc598e0fc9cd8dcd1c8caed4b
Pedro <pedro_hga@proton.me>
2024-11-28 14:57:51 -0300

    sync

--- .wezterm.lua
+++ .wezterm.lua
@@ -5,20 +5,21 @@ local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
 -- config
-config.color_scheme = "Nord (Gogh)"
--- config.color_scheme = "terafox"
+-- config.color_scheme = "Nord (Gogh)"
+config.color_scheme = "terafox"
 -- config.color_scheme = "GruvboxDark"
-config.colors = {
-	background = "#15291E",
-	-- background = "#002B36",
-}
+-- config.colors = {
+-- 	background = "#15291E",
+-- 	-- background = "#002B36",
+-- }
 
 -- config.font = wezterm.font("Iosevka Nerd Font", { weight = "Medium" })
 -- config.font = wezterm.font("MartianMono NFM", { weight = "Regular", stretch = "Condensed" })
 -- config.font = wezterm.font("Iosevka Nerd Font", { weight = "Medium" })
-config.font = wezterm.font("Iosvmata")
-config.font_size = 13.5
-config.line_height = 1.4
+-- config.font = wezterm.font("Iosvmata")
+config.font = wezterm.font("IBM Plex Mono")
+config.font_size = 12
+config.line_height = 1.25
 
 config.hide_tab_bar_if_only_one_tab = true
 -- config.window_decorations = "NONE"
