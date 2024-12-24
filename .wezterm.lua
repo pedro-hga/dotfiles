af874ca67fb7d765ad84f1751877f3196708075b
Pedro <pedro_hga@proton.me>
2024-12-24 15:45:29 -0300

    abstract machine specific components

--- .wezterm.lua
+++ .wezterm.lua
@@ -8,18 +8,20 @@ local config = wezterm.config_builder()
 -- config.color_scheme = "Nord (Gogh)"
 config.color_scheme = "terafox"
 -- config.color_scheme = "GruvboxDark"
--- config.colors = {
--- 	background = "#15291E",
--- 	-- background = "#002B36",
--- }
+config.colors = {
+	-- background = "#15291E",
+	-- background = "#002B36",
+	background = "#191D24",
+}
 
 -- config.font = wezterm.font("Iosevka Nerd Font", { weight = "Medium" })
 -- config.font = wezterm.font("MartianMono NFM", { weight = "Regular", stretch = "Condensed" })
 -- config.font = wezterm.font("Iosevka Nerd Font", { weight = "Medium" })
--- config.font = wezterm.font("Iosvmata")
-config.font = wezterm.font("IBM Plex Mono")
+config.font = wezterm.font("MonaspiceNe Nerd Font")
+-- config.font = wezterm.font("IBM Plex Mono")
 config.font_size = 12
 config.line_height = 1.25
+config.cell_width = 1
 
 config.hide_tab_bar_if_only_one_tab = true
 -- config.window_decorations = "NONE"
