5038c94e8171e09314cc4fb6600d4db1e4ff0588
Pedro <pedro_hga@proton.me>
2025-02-06 21:41:01 -0300

    changed minor ui colors

--- config/i3/scripts/screenshot.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-gnome-screenshot -a -f ~/Pictures/Screenshots/screenshot_$(date +%Y-%m-%d_%H-%M-%S).png
--- config/i3/scripts/toggle_hdmi.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-HDMI_STATUS=$(xrandr --query | grep "HDMI-0")
-
-if [[ "$HDMI_STATUS" == *"2160x3840"* ]]; then
-  xrandr --output DP-0 --primary --mode 3840x2160 --pos 0x0 --rotate normal
-else
-  xrandr --output DP-0 --primary --mode 3840x2160 --pos 0x0 --rotate normal --output HDMI-0 --mode 3840x2160 --pos 3840x192 --rotate left
-fi
