ef65f80da7537c0ab82832a5e0bd0ced3afa31fe
Pedro <pedro_hga@proton.me>
2025-07-28 11:14:22 -0300

    sync

--- .wezterm.lua
+++ .wezterm.lua
@@ -1,35 +1,13 @@
 local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
-config.color_scheme = "Nord (Gogh)"
-config.font = wezterm.font("Iosevka Nerd Font Mono", { weight = "Medium" })
-config.font_size = 14
-config.cell_width = 1.05
+config.color_scheme = "Catppuccin Frappe"
+config.font = wezterm.font("GeistMono Nerd Font")
+config.font_size = 13
+config.line_height = 1.2
+config.cell_width = 0.9
 
 config.hide_tab_bar_if_only_one_tab = true
 config.window_padding = { left = 0, right = 0, top = 6, bottom = 0 }
-
-wezterm.on("user-var-changed", function(window, pane, name, value)
-	local overrides = window:get_config_overrides() or {}
-	if name == "ZEN_MODE" then
-		local incremental = value:find("+")
-		local number_value = tonumber(value)
-		if incremental ~= nil then
-			while number_value > 0 do
-				window:perform_action(wezterm.action.IncreaseFontSize, pane)
-				number_value = number_value - 1
-			end
-			overrides.enable_tab_bar = false
-		elseif number_value < 0 then
-			window:perform_action(wezterm.action.ResetFontSize, pane)
-			overrides.font_size = nil
-			overrides.enable_tab_bar = true
-		else
-			overrides.font_size = number_value
-			overrides.enable_tab_bar = false
-		end
-	end
-	window:set_config_overrides(overrides)
-end)
 
 return config
