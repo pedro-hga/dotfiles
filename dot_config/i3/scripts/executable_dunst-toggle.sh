ea0a50d12c639194056088481f11436f1af7d400
Pedro Henrique <73372271+pedro-hga@users.noreply.github.com>
2026-08-02 20:17:21 -0300

    changed dunst layout

--- dot_config/i3/scripts/executable_dunst-toggle.sh
+++ dot_config/i3/scripts/executable_dunst-toggle.sh
@@ -1,10 +1,8 @@
 #!/usr/bin/env bash
 if [ "$(dunstctl is-paused)" = "true" ]; then
   dunstctl set-paused false
-  notify-send "🔔 Notifications Resumed"
 else
   dunstctl set-paused true
-  notify-send "🔕 Notifications Paused"
 fi
 
 polybar-msg action "#dunst.hook.0"
